98615819f69ad811e69615c7d305e3e1b20ff0a5
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2023-04-16 16:27:53 +0700

    Create ts-config.ts

---
+++ ts-config.ts
@@ -0,0 +1,10 @@
+modules.export = TS_Server = {
+    cs5: true,
+    multiplayer: true,
+}
+modules.export = CS5_Main = {
+   ext: [
+    github.markdown
+   ],
+   export: "world.zipping.palette.bot/eye.zip",
+}
